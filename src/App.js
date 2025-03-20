189957aff666fc82838d50f52a702c90f8a67678
SyntaxNate <new-shutter.nbt@gmail.com>
2025-03-20 19:12:33 -0400

    notes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,26 @@ import './App.css';
 
 
  const StepCounter = () => {
-    const [count, setCount] = useState(0);
-    const [step, setStep] = useState(1);
+    const [count, setCount] = useState(0); // Count stores the Counter value and setCount updates it
+    const [step, setStep] = useState(1); // Step Stores the Step value and setStep updates it
 
     return (
       <div style={{ textAlign: "center", marginTop: "20px" }}>
-            <h2>Counter: {count}</h2>\
+            <h2>Counter: {count}</h2>
 
             <label>
                 Step: <select onChange={(e) => setStep(Number(e.target.value))} value={step}>
-                  {[1,2,5,10].map((num) => (<option key={num} value={num}></option>))}</select>
+                        {/* This allows the user to choose how much they want to increase. setStep updates this and 
+                           e.target.value gives the selected option's value a string and we use Number() to convert it. */}
+                       {[1,2,5,10].map((num) => (<option key={num} value={num}></option>))}</select>
             </label>
 
             <div style={{ marginTop: "10px" }}>
               
                 <button onClick={() => setCount((prev) => Math.max(0, prev - step))}>Decrease</button>
+                                {/*Decreases Count by Step and uses Math.max to prevent negative numbers. */}
                 <button onClick={() => setCount((prev) => prev + step)}>Increase</button>
-
+                                {/* Increases Count by Step */}
                 <button onClick={() => setCount(0)}>Reset</button>
             </div>
       </div>
